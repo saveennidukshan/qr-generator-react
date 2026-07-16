a232f89176a5c7e710e587198894e501d1c1f8ec
Saveen Nidukshan <76433660+saveennidukshan@users.noreply.github.com>
2026-07-16 20:58:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: 'https://cdn.saveen.dev/',
+  plugins: [react(), tailwindcss()],
 })
